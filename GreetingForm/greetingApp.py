d8c1d57dbebd8fb509d3eabff5f4be1254849f83
ashna-khemani <ashna.khemani@gmail.com>
2023-05-25 15:26:06 -0700

    Special Leo Response
    Special message if "Leo" (my dog) is entered

--- GreetingForm/greetingApp.py
+++ GreetingForm/greetingApp.py
@@ -6,8 +6,15 @@ app = Flask(__name__)
 def index():
     if request.method == 'POST':
         name = request.form['thename']  # Name given to input in index.html/line9
-        return render_template('greeting.html', name=name)
+        if name == "Leo":
+            return specialLeo(name);
+        return render_template('greeting.html', name=name) # return "WASSUP %s"% name
     return render_template('index.html')
+
+@app.route("/leologin", methods=['GET', 'POST'])
+def specialLeo(usr):
+    return "HOMAIGOD IS %s" % usr
+
 
 if __name__ == '__main__':
     app.run(debug=True)
